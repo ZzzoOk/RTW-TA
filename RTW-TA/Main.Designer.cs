7741e6fe423d213c6658c625870bf41638b5ad01
Suraj Matkurbanov <zzzook@live.ru>
2019-04-01 11:15:12 +0300

    Refactored

---
+++ RTW-TA/Main.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RTW_TA
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.effectsBox = new System.Windows.Forms.ListBox();
+			this.folderBrowserDlg = new System.Windows.Forms.FolderBrowserDialog();
+			this.ancillariesBox = new System.Windows.Forms.ListBox();
+			this.descriptionBox = new System.Windows.Forms.TextBox();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.traitsBox = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// effectsBox
+			// 
+			this.effectsBox.FormattingEnabled = true;
+			this.effectsBox.ItemHeight = 16;
+			this.effectsBox.Location = new System.Drawing.Point(13, 13);
+			this.effectsBox.Margin = new System.Windows.Forms.Padding(4);
+			this.effectsBox.Name = "effectsBox";
+			this.effectsBox.Size = new System.Drawing.Size(196, 404);
+			this.effectsBox.Sorted = true;
+			this.effectsBox.TabIndex = 0;
+			this.toolTip1.SetToolTip(this.effectsBox, "Effects");
+			this.effectsBox.SelectedValueChanged += new System.EventHandler(this.effects_SelectedValueChanged);
+			// 
+			// ancillariesBox
+			// 
+			this.ancillariesBox.FormattingEnabled = true;
+			this.ancillariesBox.ItemHeight = 16;
+			this.ancillariesBox.Location = new System.Drawing.Point(217, 13);
+			this.ancillariesBox.Margin = new System.Windows.Forms.Padding(4);
+			this.ancillariesBox.Name = "ancillariesBox";
+			this.ancillariesBox.Size = new System.Drawing.Size(196, 196);
+			this.ancillariesBox.Sorted = true;
+			this.ancillariesBox.TabIndex = 1;
+			this.toolTip1.SetToolTip(this.ancillariesBox, "Ancillaries");
+			this.ancillariesBox.SelectedValueChanged += new System.EventHandler(this.ancillaries_SelectedValueChanged);
+			// 
+			// descriptionBox
+			// 
+			this.descriptionBox.Location = new System.Drawing.Point(421, 13);
+			this.descriptionBox.Margin = new System.Windows.Forms.Padding(4);
+			this.descriptionBox.Multiline = true;
+			this.descriptionBox.Name = "descriptionBox";
+			this.descriptionBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.descriptionBox.Size = new System.Drawing.Size(400, 404);
+			this.descriptionBox.TabIndex = 2;
+			this.toolTip1.SetToolTip(this.descriptionBox, "Description");
+			// 
+			// traitsBox
+			// 
+			this.traitsBox.FormattingEnabled = true;
+			this.traitsBox.ItemHeight = 16;
+			this.traitsBox.Location = new System.Drawing.Point(217, 221);
+			this.traitsBox.Margin = new System.Windows.Forms.Padding(4);
+			this.traitsBox.Name = "traitsBox";
+			this.traitsBox.Size = new System.Drawing.Size(196, 196);
+			this.traitsBox.Sorted = true;
+			this.traitsBox.TabIndex = 3;
+			this.toolTip1.SetToolTip(this.traitsBox, "Traits");
+			this.traitsBox.SelectedValueChanged += new System.EventHandler(this.traits_SelectedValueChanged);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(834, 430);
+			this.Controls.Add(this.traitsBox);
+			this.Controls.Add(this.descriptionBox);
+			this.Controls.Add(this.ancillariesBox);
+			this.Controls.Add(this.effectsBox);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.HelpButton = true;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Main";
+			this.Text = "Rome - Total War: Traits & Ancillaries";
+			this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Main_HelpButtonClicked);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox effectsBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDlg;
+        private System.Windows.Forms.ListBox ancillariesBox;
+        private System.Windows.Forms.TextBox descriptionBox;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ListBox traitsBox;
+    }
+}
+
---
+++ RTW-TA/Main.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RTW_TA
+{
+	public partial class Main : Form
+	{
+		string[] ancillaries, traits;
+
+		public Main()
+		{
+			InitializeComponent();
+
+			if (!File.Exists(Properties.Settings.Default.Path + @"\Data\export_descr_ancillaries.txt"))
+			{
+				MessageBox.Show("Укажите путь до директории \"Rome - Total War\"");
+
+				if (folderBrowserDlg.ShowDialog() == DialogResult.OK
+					&& File.Exists(folderBrowserDlg.SelectedPath + @"\Data\export_descr_ancillaries.txt"))
+				{
+					Properties.Settings.Default.Path = folderBrowserDlg.SelectedPath;
+					Properties.Settings.Default.Save();
+				}
+				else
+				{
+					MessageBox.Show("Необходимые файлы не найдены — программа завершает работу.");
+					Environment.Exit(1);
+				}
+			}
+
+			try
+			{
+				var descr = File.ReadAllLines(Properties.Settings.Default.Path + @"\Data\export_descr_ancillaries.txt");
+				var ancillariesDesc =
+					File.ReadAllLines(Properties.Settings.Default.Path + @"\Data\text\export_ancillaries.txt");
+
+				foreach (var s in descr)
+					if (s.Contains("Effect "))
+					{
+						var effect = s.Split(' ')[5];
+
+						if (!effectsBox.Items.Contains(effect))
+							effectsBox.Items.Add(effect);
+					}
+
+				string temp;
+				int i = 0, j = 0;
+
+				ancillaries = new string[ancillariesDesc.Count(s => s.Contains("_effects_desc}"))];
+				while (i < descr.Length)
+				{
+					if (descr[i].StartsWith("Ancillary "))
+					{
+						temp = '{' + descr[i].Substring(descr[i].LastIndexOf(' ') + 1) + "_effects_desc}";
+
+						while (descr[i] != "")
+							ancillaries[j] += descr[i++] + "\r\n";
+
+						for (var k = 0; k < ancillariesDesc.Length; ++k)
+						{
+							if (!ancillariesDesc[k].Contains(temp)) continue;
+
+							ancillaries[j] += "\r\n" + ancillariesDesc[k + 1].Replace(", ", "\r\n") + "\r\n";
+
+							break;
+						}
+
+						++j;
+					}
+
+					++i;
+				}
+
+				descr = File.ReadAllLines(Properties.Settings.Default.Path + @"\Data\export_descr_character_traits.txt");
+				var traitsDesc = File.ReadAllLines(Properties.Settings.Default.Path + @"\Data\text\export_VnVs.txt");
+
+				foreach (var s in descr)
+					if (s.Contains("Effect "))
+					{
+						var trait = s.Split(' ')[5];
+
+						if (!effectsBox.Items.Contains(trait))
+							effectsBox.Items.Add(trait);
+					}
+
+				i = j = 0;
+				traits = new string[traitsDesc.Count(s => s.Contains("_effects_desc}"))];
+				while (i < descr.Length)
+				{
+					if (descr[i].StartsWith("Trait "))
+					{
+						while (descr[i] != "" || descr[i + 1] != "")
+						{
+							traits[j] += descr[i] + "\r\n";
+
+							if (descr[i].Contains(" Level "))
+							{
+								temp = '{' + descr[i].Substring(descr[i].LastIndexOf(' ') + 1) + "_effects_desc}";
+
+								if (descr[i].Contains(" Effect ") && descr[i + 1] == "")
+								{
+									for (var k = 0; k < traitsDesc.Length; ++k)
+										if (traitsDesc[k].Contains(temp))
+										{
+											traits[j] += "\r\n" + traitsDesc[k + 1].Replace(", ", "\r\n") + "\r\n";
+											break;
+										}
+								}
+							}
+
+							++i;
+						}
+
+						++j;
+					}
+
+					++i;
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void effects_SelectedValueChanged(object sender, EventArgs e)
+		{
+			if (effectsBox.SelectedIndex != -1)
+			{
+				ancillariesBox.Items.Clear();
+				traitsBox.Items.Clear();
+
+				foreach (var s in ancillaries)
+					if (s != null && s.Contains(effectsBox.SelectedItem.ToString()))
+						ancillariesBox.Items.Add(s.Split(' ')[1].Replace("\r\n", ""));
+
+				foreach (var s in traits)
+					if (s != null && s.Contains(effectsBox.SelectedItem.ToString()))
+						traitsBox.Items.Add(s.Split(' ')[1].Replace("\r\n", "") + ' ' + Regex.Matches(s, " Level ").Count);
+			}
+		}
+
+		private void ancillaries_SelectedValueChanged(object sender, EventArgs e)
+		{
+			if (ancillariesBox.SelectedIndex != -1)
+			{
+				descriptionBox.Text = "";
+				foreach (var s in ancillaries)
+					if (s != null && s.Contains("Ancillary " + ancillariesBox.SelectedItem + "\r\n"))
+					{
+						descriptionBox.Text = s;
+						break;
+					}
+			}
+		}
+
+		private void traits_SelectedValueChanged(object sender, EventArgs e)
+		{
+			if (traitsBox.SelectedIndex != -1)
+			{
+				descriptionBox.Text = "";
+				foreach (var s in traits)
+					if (s != null && s.Contains("Trait " + traitsBox.SelectedItem.ToString()
+													.Remove(traitsBox.SelectedItem.ToString().IndexOf(' '))))
+					{
+						descriptionBox.Text = s;
+						break;
+					}
+			}
+		}
+
+		private void Main_HelpButtonClicked(object sender, CancelEventArgs e)
+		{
+			MessageBox.Show("В программе задействованы всплывающие подсказки — наведите курсор на интересующий элемент.");
+		}
+	}
+}
